04e57d18769d4a18d5b9a0c18f5ba3a054a3420c
coolevil98 <68813255+coolevil98@users.noreply.github.com>
2022-09-20 12:30:39 +1000

    Stuff

---
+++ My project/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    private float space=0;
+    private float timer = 0;
+    private float jump = 0;
+    private float jumpHeight = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer>=0)
+        {
+            timer -= Time.deltaTime;
+            jump = 1;
+        }
+        float dx = Input.GetAxis("Horizontal");
+        float dy =Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(dx, jump, dy) * moveSpeed * Time.deltaTime);
+
+        //jump += jumpHeight * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            timer = 2;
+           // transform.Translate(new Vector3(dx, jumpHeight, dy) * moveSpeed * Time.deltaTime);
+
+        }
+        jump = 0;
+
+    }
+}
